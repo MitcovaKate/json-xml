947f288e37cd702219491aa50dcb66f92722b04b
Mitcova Kate <katya.mitkova@bk.ru>
2024-01-27 17:18:30 +0200

    Update app.js

--- autocomplite/app.js
+++ autocomplite/app.js
@@ -15,20 +15,32 @@ localStorage.setItem(`formDta-${data.name}`, jsonData)
 
 const loadFromData=(e)=>{
       let input =e.target
+      input.value = input.value.toUpperCase();
       let name=input.value
-      let foundKey=Object.keys(localStorage).find(key=>key.startsWith("formData-") && key.endsWith(name)
-      )
- if (foundKey && name.length > 0) {
-    let data = JSON.parse(localStorage.getItem(foundKey));
-    let matchingValues = Object.values(data).filter(
-      (value) => value.startsWith(name)
-    );
 
-    if (matchingValues.length > 0) {
-      input.innerHTML+=matchingValues
-     
-    }
+      let foundKey=Object.keys(localStorage).find(
+                      key=>key.startsWith("formData-") && key.endsWith(name)
+       )
+       
+if (foundKey && name.length > 0) {
+    let data = JSON.parse(localStorage.getItem(foundKey))
+    
+Object.keys(data).forEach(key=>{
+                   let value = data[key]
+                  document.querySelector(`#${key}`).value = value
+                })
+    
   }
-};
+ else{
+
+  let inputs = form.querySelectorAll('input')
+   inputs.forEach((input) => {
+      input.value = ""
+   })  
+ }
+
 }
-document.forms[0].addEventListener('submit', saveFormData)
+
+
+document.forms[0].addEventListener('submit',saveFormData)
+document.querySelector('form [id="name"]').addEventListener('keyup',loadFromData)
